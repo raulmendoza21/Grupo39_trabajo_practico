f45d83b2f25691a6324e07fbb3dda48574c758f6
Aris64 <arissoriano71@gmail.com>
2023-10-13 10:37:02 +0100

    faltan cosas

--- src/main/java/org/ulpgc/is1/model/Address.java
+++ src/main/java/org/ulpgc/is1/model/Address.java
@@ -1,4 +1,15 @@
 package org.ulpgc.is1.model;
 
 public class Address {
+    public String street;
+    public int number;
+    public int postalCode;
+    public String city;
+
+    public Address(String street, int number, int postalCode, String city) {
+        this.street = street;
+        this.number = number;
+        this.postalCode = postalCode;
+        this.city = city;
+    }
 }
