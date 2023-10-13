9583973b0fc473b867da38df667448bd103ef8df
raule <raulelectrico21@gmail.com>
2023-10-13 10:30:43 +0100

    Class Reservation

--- src/main/java/org/ulpgc/is1/model/CourtType.java
+++ src/main/java/org/ulpgc/is1/model/CourtType.java
@@ -1,6 +1,5 @@
 package org.ulpgc.is1.model;
 
  enum CourtType {
-
     Slow_court, Fast_court
 }
--- src/main/java/org/ulpgc/is1/model/Reservation.java
+++ src/main/java/org/ulpgc/is1/model/Reservation.java
@@ -3,5 +3,13 @@ package org.ulpgc.is1.model;
 public class Reservation {
 
 	private Court court;
+	private Date Date;
+	private static int Next_ID = 0;
+	final int Id;
 
+	public Reservation(Court court, Date date, int id) {
+		this.court = court;
+		Date = date;
+		Id = id;
+	}
 }
