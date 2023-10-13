59d0ebc50bc21c83ebb34cab5013f9775c3a92be
himarr <denizhimar@gmail.com>
2023-10-13 10:09:02 +0100

    Implementada clase abstracta Extra y las clases Umpire y Equipment que heredan de esta

--- src/main/java/org/ulpgc/is1/model/Equipment.java
+++ src/main/java/org/ulpgc/is1/model/Equipment.java
@@ -1,4 +1,19 @@
 package org.ulpgc.is1.model;
 
-public class Equipment {
+public class Equipment extends Extra {
+
+    private String name;
+
+    public Equipment(int price, String  name) {
+        super(price);
+        this.name = name;
+    }
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
